0ba60e1e0fe173553e9fb2b27745a3fe06639406
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2024-11-01 21:08:43 +0900

    ✨ initial commit

---
+++ src/main/java/com/example/GDSC_insight/GdscInsightApplication.java
@@ -0,0 +1,13 @@
+package com.example.GDSC_insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscInsightApplication.class, args);
+	}
+
+}
