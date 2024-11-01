621b9b1eb3d4a4697ace8962e7bb71d8fcb099aa
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2024-11-02 06:17:36 +0900

    Revert "feat : 회원가입 로그인 기능 구현"

--- src/main/java/com/example/GDSC_insight/GdscInsightApplication.java
+++ src/main/java/com/example/GDSC_insight/GdscInsightApplication.java
@@ -2,10 +2,8 @@ package com.example.GDSC_insight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class GdscInsightApplication {
 
 	public static void main(String[] args) {
